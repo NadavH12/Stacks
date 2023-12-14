306843cd3ac1f83789a5ad207b191f2d71a00210
Nadav Horowitz <115586314+NadavH12@users.noreply.github.com>
2023-12-14 11:28:59 -0800

    Add files via upload

---
+++ LinkedStack.cpp
@@ -0,0 +1,83 @@
+/** @file LinkedStack.cpp */
+#include <cassert> // For assert
+#include "LinkedStack.h" // Header file
+template< class ItemType>
+
+LinkedStack<ItemType>::LinkedStack() : topPtr( nullptr){
+} // end default constructor
+
+template< class ItemType>
+LinkedStack<ItemType>::LinkedStack( const LinkedStack<ItemType>& aStack){
+
+    // Point to nodes in original chain
+    Node<ItemType>* origChainPtr = aStack->topPtr;
+    if (origChainPtr == nullptr)
+        topPtr = nullptr; // Original bag is empty
+    else{
+        // Copy first node
+        topPtr = new Node<ItemType>();
+        topPtr->setItem(origChainPtr->getItem());
+        // Point to first node in new chain
+        Node<ItemType>* newChainPtr = topPtr;
+        // Copy remaining nodes
+
+        while (origChainPtr != nullptr){
+
+            // Advance original-chain pointer
+            origChainPtr = origChainPtr->getNext();
+            // Get next item from original chain
+            ItemType nextItem = origChainPtr->getItem();
+            // Create a new node containing the next item
+            Node<ItemType>* newNodePtr = new Node<ItemType>(nextItem);
+            // Link new node to end of new chain
+            newChainPtr->setNext(newNodePtr);
+            // Advance pointer to new last node
+            newChainPtr = newChainPtr->getNext();
+        }
+
+        newChainPtr->setNext( nullptr); // Flag end of chain
+    }
+}
+
+template< class ItemType>
+LinkedStack<ItemType>::~LinkedStack(){
+    // Pop until stack is empty
+    while (!isEmpty())
+        pop();
+}
+
+template< class ItemType>
+bool LinkedStack<ItemType>::isEmpty() const{
+    return topPtr == nullptr;
+}
+
+template< class ItemType>
+bool LinkedStack<ItemType>::push( const ItemType& newItem){
+    Node<ItemType>* newNodePtr = new Node<ItemType>(newItem, topPtr);
+    topPtr = newNodePtr;
+    newNodePtr = nullptr;
+    return true;
+}
+
+template< class ItemType>
+bool LinkedStack<ItemType>::pop(){
+    bool result = false;
+    if (!isEmpty()){
+        // Stack is not empty; delete top
+        Node<ItemType>* nodeToDeletePtr = topPtr;
+        topPtr = topPtr->getNext();
+        // Return deleted node to system
+        nodeToDeletePtr->setNext( nullptr);
+        delete nodeToDeletePtr;
+        nodeToDeletePtr = nullptr;
+        result = true;
+    }
+    return result;
+}
+
+template< class ItemType>
+ItemType LinkedStack<ItemType>::peek() const{
+    assert(!isEmpty()); // Enforce precondition
+    // Stack is not empty; return top
+    return topPtr->getItem();
+}
---
+++ Stack.cpp
@@ -0,0 +1,23 @@
+#ifndef STACK_
+#define STACK_
+
+#include "LinkedStack.h"
+template <class T>
+class Stack {
+    public:
+        T max() const{
+            return NULL;
+
+        }
+
+        T mean() const{
+            return NULL;
+
+        }
+
+        Stack<T> operator+(const Stack<T> &other){
+            return NULL;
+        }
+
+};
+#endif
